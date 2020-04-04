f57e3f5f6695bd8118c3eb83a3c0522cdfed1be2
Joel Hooks <joelhooks@gmail.com>
2020-04-04 15:21:21 -0700

    more of the same

--- functions/process-url/process-url.js
+++ functions/process-url/process-url.js
@@ -8,6 +8,7 @@ cloudinary.config({
 
 exports.handler = async function(event, ctx) {
   const {queryStringParameters} = event
+  console.log(queryStringParameters)
   try {
     // https://res.cloudinary.com/dg3gyk0gu/image/upload/v1586032616/og-images/pixel.png
     const imageUrl = cloudinary.url(`v1586034934/og_images/pixel_zeddhz.png`, {
@@ -17,10 +18,16 @@ exports.handler = async function(event, ctx) {
       custom_pre_function: {
         function_type: 'remote',
         source: `https://competent-goodall-d71d0d.netlify.com/.netlify/functions/gen-opengraph-image?${qs.stringify(
-          {title: 'butts', author: 'Joel Hooks', tags: '1, 2, 3'},
+          queryStringParameters,
         )}`,
       },
     })
+
+    console.log(
+      `https://competent-goodall-d71d0d.netlify.com/.netlify/functions/gen-opengraph-image?${qs.stringify(
+        queryStringParameters,
+      )}`,
+    )
     return {
       statusCode: 302,
       headers: {
