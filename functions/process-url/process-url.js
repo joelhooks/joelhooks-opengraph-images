aa83bad51b40942cb93a25f210a2ba0411988f61
Joel Hooks <joelhooks@gmail.com>
2020-04-04 15:11:00 -0700

    try harder

--- functions/process-url/process-url.js
+++ functions/process-url/process-url.js
@@ -8,10 +8,6 @@ cloudinary.config({
 
 exports.handler = async function(event, ctx) {
   const {queryStringParameters} = event
-  const query = qs.stringify(queryStringParameters).replace('=', '%3D')
-  const sourceUrl = `https://competent-goodall-d71d0d.netlify.com/.netlify/functions/gen-opengraph-image`
-  console.log('source url:', sourceUrl)
-  console.log('query: ', query)
   try {
     // https://res.cloudinary.com/dg3gyk0gu/image/upload/v1586032616/og-images/pixel.png
     const imageUrl = cloudinary.url(`v1586034934/og_images/pixel_zeddhz.png`, {
@@ -20,10 +16,11 @@ exports.handler = async function(event, ctx) {
       // secure: true,
       custom_pre_function: {
         function_type: 'remote',
-        source: sourceUrl,
+        source: `https://competent-goodall-d71d0d.netlify.com/.netlify/functions/gen-opengraph-image?${qs.stringify(
+          queryStringParameters,
+        )}`,
       },
     })
-    console.log('image url:', imageUrl)
     return {
       statusCode: 302,
       headers: {
