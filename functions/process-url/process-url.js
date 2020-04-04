c2c193ec145e700e2a6114d866f4272d9907ea70
Joel Hooks <joelhooks@gmail.com>
2020-04-04 12:40:19 -0700

    add the cloudname env var

--- functions/process-url/process-url.js
+++ functions/process-url/process-url.js
@@ -1,7 +1,7 @@
 const cloudinary = require('cloudinary').v2
 const qs = require('querystring')
 cloudinary.config({
-  cloud_name: 'sector',
+  cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUDINARY_KEY,
   api_secret: process.env.CLOUDINARY_SECRET,
 })
@@ -12,14 +12,16 @@ exports.handler = async function(event, ctx) {
   try {
     // https://res.cloudinary.com/dg3gyk0gu/image/upload/v1586028554/jh-og-image/1px.png
     const imageUrl = cloudinary.url(
-      `${process.env.JOEL_IMAGE_VERSION}/og-images/img-1.png`,
+      `${process.env.JOEL_IMAGE_VERSION}/jh-og-image/1px.png`,
       {
         // resouce_type: "raw"
         sign_url: true,
         // secure: true,
         custom_pre_function: {
           function_type: 'remote',
-          source: `https://competent-goodall-d71d0d.netlify.app/.netlify/functions/gen-opengraph-image?${qs.stringify(
+          source: `${
+            process.env.DEPLOY_PRIME_URL
+          }/.netlify/functions/gen-opengraph-image?${qs.stringify(
             queryStringParameters,
           )}`,
         },
