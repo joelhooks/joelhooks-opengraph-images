8e0cd45f111dfdf08ca302699f471c1bbbd1392b
Joel Hooks <joelhooks@gmail.com>
2020-04-04 16:03:23 -0700

    try fetch instead

--- functions/process-url/process-url.js
+++ functions/process-url/process-url.js
@@ -6,22 +6,30 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_SECRET,
 })
 
-exports.handler = async function(event, ctx) {
+exports.handler = async function (event, ctx) {
   const {queryStringParameters} = event
   console.log(queryStringParameters)
   try {
     // https://res.cloudinary.com/dg3gyk0gu/image/upload/v1586032616/og-images/pixel.png
-    const imageUrl = cloudinary.url(`v1586034934/og_images/pixel_zeddhz.png`, {
-      // resouce_type: "raw"
-      sign_url: true,
-      // secure: true,
-      custom_pre_function: {
-        function_type: 'remote',
-        source: `https://competent-goodall-d71d0d.netlify.com/.netlify/functions/gen-opengraph-image?${qs.stringify(
-          queryStringParameters,
-        )}`,
-      },
-    })
+    // const imageUrl = cloudinary.url(`v1586034934/og_images/pixel_zeddhz.png`, {
+    //   // resouce_type: "raw"
+    //   sign_url: true,
+    //   // secure: true,
+    //   custom_pre_function: {
+    //     function_type: 'remote',
+    //     source: `https://competent-goodall-d71d0d.netlify.com/.netlify/functions/gen-opengraph-image?${qs.stringify(
+    //       queryStringParameters,
+    //     )}`,
+    //   },
+    // })
+
+    const imageUrl = `https://res.cloudinary.com/${
+      process.env.CLOUD_NAME
+    }/image/fetch${encodeURI(
+      `https://competent-goodall-d71d0d.netlify.com/.netlify/functions/gen-opengraph-image?${qs.stringify(
+        queryStringParameters,
+      )}`,
+    )}`
 
     console.log(
       `https://competent-goodall-d71d0d.netlify.com/.netlify/functions/gen-opengraph-image?${qs.stringify(
